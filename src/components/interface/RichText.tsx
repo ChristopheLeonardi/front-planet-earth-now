6d73a1b45b686d74ba91722eec432acfd6d5ff51
christopheLeonardi <christophe.leonardi1@gmail.com>
2025-01-24 15:04:03 +0100

    cleaning

--- src/components/interface/RichText.tsx
+++ src/components/interface/RichText.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { BlocksRenderer, type BlocksContent } from '@strapi/blocks-react-renderer';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 interface RichTextProps { data: any; }
 
 const RichText: React.FC<RichTextProps> = ({ data }) => {
   const content: BlocksContent = data;
+
     useEffect(() => {
-    let paragraph = document.querySelectorAll(".rich-text-block p");
-    let container = document.createElement("div")
-    container.setAttribute("class","text-container")
-    Array.from(document.querySelectorAll(".rich-text-block + p")).map(elt => elt.innerHTML = "")
-    Array.from(paragraph).map(p => { return container.appendChild(p)})
-    let button = document.getElementById("bouton-flag")
-    document.getElementById("rich-container")?.appendChild(container)
-    if(button){
-      container.appendChild(button)
 
-    }
+      let paragraph = document.querySelectorAll(".rich-text-block p");
 
-    let textContainer = document.querySelectorAll(".text-container")
-    let filtered = Array.from(textContainer).filter(c => c.children.length === 0)
-    filtered.map(f => f.remove())
-    console.log(filtered)
+      let container = document.createElement("div")
+      container.setAttribute("class","text-container")
+
+      Array.from(document.querySelectorAll(".rich-text-block + p")).map(elt => elt.innerHTML = "")
+
+      Array.from(paragraph).map(p => { return container.appendChild(p)})
+
+      document.getElementById("rich-container")?.appendChild(container)
+      
+      let button = document.getElementById("bouton-flag")
+      if(button){ container.appendChild(button) }
+
+      let textContainer = document.querySelectorAll(".text-container")
+      let filtered = Array.from(textContainer).filter(c => c.children.length === 0)
+      filtered.map(f => f.remove())
+
     }, [])
 
 
